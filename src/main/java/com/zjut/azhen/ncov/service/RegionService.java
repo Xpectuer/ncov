6ae33160b4b081afc127a36a0b58ea081734356c
AW <yzh2520@126.com>
2020-06-05 17:46:23 +0800

    完成所有关于redis部分的修改，并且构造了新的redisTemplate类。 但是具体在getRegion()其他地方由于没怎么看懂并没有使用redis缓存技术，数据量也比较小，可用可不用。

--- src/main/java/com/zjut/azhen/ncov/service/RegionService.java
+++ src/main/java/com/zjut/azhen/ncov/service/RegionService.java
@@ -3,8 +3,16 @@ package com.zjut.azhen.ncov.service;
 
 import com.zjut.azhen.ncov.bean.Region;
 import com.zjut.azhen.ncov.mapping.RegionMapper;
+import com.zjut.azhen.ncov.redisConfig.redisUtil;
+import com.zjut.azhen.ncov.redisConfig.redisConfig;
 import org.springframework.stereotype.Service;
 import java.util.List;
+
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+
+
 /*
 *
 *   Region Service
@@ -14,12 +22,26 @@ import java.util.List;
 public class RegionService {
 
     private RegionMapper regionMapper;
+    private redisUtil redisTemplate;
 
     public RegionService(RegionMapper mapper){
         regionMapper=mapper;
     }
 
     public Region getRegionRealtimeById(int id){
+        String key = "User" + id;
+
+        ValueOperations<String, Region> operations = null;
+
+        boolean hasKey = redisTemplate.hasKey(key);
+
+        if(hasKey){
+            Region region = operations.get(key);
+            return region;
+        }else{
+            Region region=regionMapper.getRegionRealtimeById(id);
+            operations.set(key, region, 5);
+        }
         Region region=regionMapper.getRegionRealtimeById(id);
        System.out.println(region.getConfirmed());
         return region;
